fe9c2db7824657ac6f5c152a9b301daf3e9b5824
Hereman Nicolas <nhereman@ulb.ac.be>
2015-04-30 13:23:30 +0200

    Ajout de l'interface statique d'enregistrement

--- solution/15-05-15/windowvillo.py
+++ solution/15-05-15/windowvillo.py
@@ -28,7 +28,8 @@ class WindowVillo(Frame):
                                 command=self.__makeConsultpage)
         self.registerButton = Button(self,
                                 text="S'enregistrer",
-                                width=50)
+                                width=50,
+                                command=self.__makeRegistrypage)
         self.connectButton = Button(self,
                                text="Se connecter",
                                width=50)
@@ -45,6 +46,8 @@ class WindowVillo(Frame):
             self.__destroyHomepage()
         elif ( self.page == "consult" ):
             self.__destroyConsultpage()
+        elif ( self.page == "registry" ):
+            self.__destroyRegistrypage()
         self.page = ""
 
     def __destroyHomepage(self):
@@ -89,4 +92,128 @@ class WindowVillo(Frame):
         self.listVillo.destroy()
         self.buttonVillo.destroy()
         self.buttonBack.destroy()
+
+    def __makeRegistrypage(self):
+        """ Dessine la page d'enregistrement """
+        self.__destroyPage()
+        self.page = "registry"
+
+        # Création widgets
+        self.idEntry = Entry(self)
+        self.idLabel = Label(self, text="ID :")
+
+        self.passEntry = Entry(self, show="*")
+        self.passLabel = Label(self, text="Mot de passe :")
+
+        self.passValEntry = Entry(self, show="*")
+
+
+        self.firstnameEntry = Entry(self)
+        self.firstnameLabel = Label(self, text="Prénom :")
+
+        self.nameEntry = Entry(self)
+        self.nameLabel = Label(self, text="Nom :")
+
+        self.phoneEntry = Entry(self)
+        self.phoneLabel = Label(self, text="Téléphone :")
+
+        self.cityEntry = Entry(self)
+        self.cityLabel = Label(self, text="Ville :")
+
+        self.cpEntry = Entry(self)
+        self.cpLabel = Label(self, text="Code postal :")
+
+        self.streetEntry = Entry(self)
+        self.streetLabel = Label(self, text="Rue :")
+
+        self.numberEntry = Entry(self)
+        self.numberLabel = Label(self, text="Numéro :")
+
+        self.cardEntry = Entry(self)
+        self.cardLabel = Label(self, text="Carte bancaire :")
+
+        self.registryButton = Button(self,
+                                     text="S'enregistrer",
+                                     width=40)
+
+
+        self.buttonBack = Button(self,
+                                 text="Retour",
+                                 command=self.__makeHomepage)
+
+
+        # Placement widgets
+        self.idEntry.place(x=420,y=70)
+        self.idLabel.place(x=280,y=70)
         
+        self.passEntry.place(x=420,y=100)
+        self.passLabel.place(x=280,y=100)
+
+        self.passValEntry.place(x=420,y=130)
+
+        self.firstnameEntry.place(x=420,y=160)
+        self.firstnameLabel.place(x=280,y=160)
+
+        self.nameEntry.place(x=420,y=190)
+        self.nameLabel.place(x=280,y=190)
+
+        self.phoneEntry.place(x=420,y=220)
+        self.phoneLabel.place(x=280,y=220)
+
+        self.cityEntry.place(x=420,y=250)
+        self.cityLabel.place(x=280,y=250)
+
+        self.cpEntry.place(x=420,y=280)
+        self.cpLabel.place(x=280,y=280)
+
+        self.streetEntry.place(x=420,y=310)
+        self.streetLabel.place(x=280,y=310)
+
+        self.numberEntry.place(x=420,y=340)
+        self.numberLabel.place(x=280,y=340)
+
+        self.cardEntry.place(x=420, y=370)
+        self.cardLabel.place(x=280, y=370)
+
+        self.registryButton.place(x=280,y=420)
+
+        self.buttonBack.place(x=50,y=550)
+
+
+    def __destroyRegistrypage(self):
+        """ Détruit la page d'enregistrement """
+        self.idEntry.destroy()
+        self.idLabel.destroy()
+        
+        self.passEntry.destroy()
+        self.passLabel.destroy()
+
+        self.passValEntry.destroy()
+
+        self.firstnameEntry.destroy()
+        self.firstnameLabel.destroy()
+
+        self.nameEntry.destroy()
+        self.nameLabel.destroy()
+
+        self.phoneEntry.destroy()
+        self.phoneLabel.destroy()
+
+        self.cityEntry.destroy()
+        self.cityLabel.destroy()
+
+        self.cpEntry.destroy()
+        self.cpLabel.destroy()
+
+        self.streetEntry.destroy()
+        self.streetLabel.destroy()
+
+        self.numberEntry.destroy()
+        self.numberLabel.destroy()
+
+        self.cardEntry.destroy()
+        self.cardLabel.destroy()
+
+        self.registryButton.destroy()
+
+        self.buttonBack.destroy()
